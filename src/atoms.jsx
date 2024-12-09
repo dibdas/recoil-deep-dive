e32bfb1a1bb5863d2b7b8a6945447aaa4561fc7e
dibdas <dibyandu08das@gmail.com>
2024-12-09 20:16:54 +0530

    modified

--- src/atoms.jsx
+++ src/atoms.jsx
@@ -25,6 +25,47 @@ export const meAtom = atom({
   default: 0,
 });
 
+// export const notifications = atom({
+//   key: "notificationAtom",
+//   default: {
+//     network: 4,
+//     jobs: 6,
+//     messaging: 3,
+//     notification: 7,
+//   },
+// });
+
+export const notifications = atom({
+  key: "notificationAtom",
+  //   default: async () => {
+  //     const res = await axios.get("https://sum-server.100xdevs.com/notification");
+  //     return res.data;
+  //   },
+  default: selector({
+    key: "networkAtomSelector",
+    get: async () => {
+      //setting an artificial delay here
+      //   screen will be blank white for 5 seconds
+      await new Promise((r) => setTimeout(r, 5000));
+      const res = await axios.get(
+        "https://sum-server.100xdevs.com/notification"
+      );
+      return res.data;
+    },
+  }),
+});
+export const totalSelector = selector({
+  key: "totalSelector",
+  get: ({ get }) => {
+    const allNotification = get(notifications);
+    return (
+      allNotification.messaging +
+      allNotification.jobs +
+      allNotification.notification +
+      allNotification.network
+    );
+  },
+});
 export const totalNotificationSelector = selector({
   key: "totalNotificationSelector",
   get: ({ get }) => {
