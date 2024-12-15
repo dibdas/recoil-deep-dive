b2979c923a7847374a3094e492ee2be1c574c19c
dibdas <dibyandu08das@gmail.com>
2024-12-15 22:16:24 +0530

    modified

--- src/Selectorfamily.jsx
+++ src/Selectorfamily.jsx
@@ -10,10 +10,35 @@ export const todosAtomfamily = atomFamily({
     get:
       (id) =>
       async ({ get }) => {
+        // delaying the request for  5 seconds
+        await new Promise(r =>setTimeout(r,5000))
         const res = await axios.get(
           `https://sum-server.100xdev.com/todod?id=%${id}`
         );
         return res.data.todo;
       },
+  }),
+});
+
+
+// easier syntax
+
+
+export const todosAtomfamily = atomFamily({
+  key: "todosfamily",
+  //it tells which should be rendered
+  //   if id =1 below logic will run for id =1
+  //   if id =2 , below logic will run for id =2
+  default: selectorFamily({
+    key: "todoselectorfamily",
+    get:
+      function(id) {
+      return async function({ get }) {
+        const res = await axios.get(
+          `https://sum-server.100xdev.com/todod?id=%${id}`
+        );
+        return res.data.todo;
+      },
+    }
   }),
 });
